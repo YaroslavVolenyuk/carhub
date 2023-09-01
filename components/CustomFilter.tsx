88e9eadcc1ab379388788d41f72c3fe28d88eff5
Yaroslav Volenyuk <volenyuk.yaroslav@gmail.com>
2023-09-01 22:56:12 +0200

    Add filters

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+'use client';
 
-const CustomFilter = () => {
-  return <div>CustomFilter</div>;
+import { Listbox, Transition } from '@headlessui/react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Fragment, useState } from 'react';
+import { CustomFilterProps } from '../types';
+
+useRouter;
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-fit">
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+        <div className="relative w-fit z-10 ">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="up-down"
+            />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          ></Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default CustomFilter;
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,14 @@
-import { CarProps } from '../types';
+import { CarProps, FilterProps } from '../types';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
     'X-RapidAPI-Key': 'ea34ebfb9emshff886196d8a0b21p1afc54jsn33fa9ede6240',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     { headers: headers },
   );
 
