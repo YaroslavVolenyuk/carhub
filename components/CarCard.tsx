c9096bb87edde30ae0a822567a68f1cc27e32d5c
Yaroslav Volenyuk <volenyuk.yaroslav@gmail.com>
2023-09-03 15:02:40 +0200

    Fix types and API

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { CarProps } from '../types/index';
-import { calculateCarRent, generateCarImageUrl } from '../utils/index';
+import { calculateCarRent } from '../utils/index';
 import { CustomButton } from './';
 import CarDetails from './CarDetails';
 
@@ -12,68 +12,93 @@ interface CarCardProps {
 }
 
 const CarCard = ({ car }: CarCardProps) => {
+  const [isLiked, setIsLiked] = useState(false);
   const { city_mpg, year, make, model, transmission, drive } = car;
-  const carRent = calculateCarRent(city_mpg, year);
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const imaginApiKey = 'hrjavascript-mastery';
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
         <h2 className="car-card__content-title">
           {make} {model}
         </h2>
+
+        <Image
+          src={!isLiked ? '/heart-outline.svg' : '/heart-filled.svg'}
+          width={24}
+          height={24}
+          alt="heart"
+          className="object-contain cursor-pointer mt-0.5"
+          onClick={() => setIsLiked(!isLiked)}
+        />
       </div>
-      <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">$</span>
+
+      <p className="car-card__price">
+        <span className="car-card__price-dollar">$</span>
         {carRent}
-        <span className="self-end text-[14px] font-medium">/day</span>
+        <span className="car-card__price-day">/day</span>
       </p>
 
-      <div className="relative w-full h-40 my-3 object-contain">
+      <div className="car-card__image">
         <Image
-          src={generateCarImageUrl(car)}
+          src={`https://cdn.imagin.studio/getimage?customer=${imaginApiKey}&make=${make}&modelFamily=${
+            model.split(' ')[0]
+          }&zoomType=fullscreen&modelYear=${year}`}
           alt="car model"
           fill
           priority
           className="object-contain"
         />
       </div>
+
       <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
-          <div className="flex flex-col justify-center items-center gap-2">
+        <div className="car-card__icon-container">
+          <div className="car-card__icon">
             <Image
               src="/steering-wheel.svg"
               width={20}
               height={20}
-              alt="wheel"
+              alt="steering wheel"
             />
-            <p className="text-[14px]">
+            <p className="car-card__icon-text">
               {transmission === 'a' ? 'Automatic' : 'Manual'}
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/tire.svg" width={20} height={20} alt="tire" />
-            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/gas.svg" width={20} height={20} alt="gas" />
-            <p className="text-[14px]">{city_mpg} MPG</p>
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
+
         <div className="car-card__btn-container">
           <CustomButton
             title="View More"
             containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
-            textStyles="text-white text-[14px] leading-[17px]"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
-            handleClick={() => setIsOpen(true)}
+            handleClick={openModal}
           />
         </div>
       </div>
-      <CarDetails
-        isOpen={isOpen}
-        closeModal={() => setIsOpen(false)}
-        car={car}
-      />
+
+      <CarDetails isOpen={isOpen} closeModal={closeModal} car={car} />
     </div>
   );
 };
--- utils/index.ts
+++ utils/index.ts
@@ -54,3 +54,18 @@ export async function fetchCars(filters: FilterProps) {
   const result = await response.json();
   return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery' || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+};
